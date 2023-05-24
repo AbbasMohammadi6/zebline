442324089203f29daac7f97e3d3ac2e640239ac7
AbbasMohammadi <abbasmohammadi6@gmail.com>
2023-05-24 09:21:47 +0330

    add login btn

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react'
 
+const token = 'bearer 4$U2VnbWVudCFuMA$TyBLdeYwC71684835503713';
+const licence_code = '4$U2VnbWVudCFuMA$SCA9IOb4gf1684835503794';
+
 const Index = () => {
+  const loginUser = async () => {
+    await fetch(`{HOST}/v1/accounts/${licence_code}/users`, {
+      method: 'POST',
+      body: JSON.stringify({ name: 'test' }),
+      headers: { Authorization: token }
+    });
+  }
+
   return (
-    <div>
-     The main page 
-    </div>
+    <button onClick={loginUser}>Login</button>
   )
 }
 
